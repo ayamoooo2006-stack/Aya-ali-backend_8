a38590f6714e86c2ffdbdff7eec0141159f69ed5
Berry Mohamed Ali <ayamoooo2006@gmail.com>
2025-09-20 00:15:02 +0300

    Add files via upload

---
+++ 0001.py
@@ -0,0 +1,14 @@
+# create user info form
+first_name = "Aya" 
+last_name = "Ali"
+GPA = 3.5
+date_of_birth = "31/3/2006"
+age = 19
+
+# طباعة كل البيانات
+print("\n--- User Info ---")
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("GPA:", GPA)
+print("Date of Birth:", date_of_birth)
+print("Age:", age)
